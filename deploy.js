7a69b2e082069d170223d14c9b2ce617d5dcdeb2
ssarcandy <ssarcandy@gmail.com>
2016-04-07 13:16:37 -0400

    fix

--- deploy.js
+++ deploy.js
@@ -1,14 +1,14 @@
 var cp = require('child_process');
 var path = require('path');
 var os = require('os');
-//var working_path = path.resolve(__dirname, './');
+var appDir;
 
 var out = str => {
 	console.log(str);
 };
 
 var job = (str, options) => {
-	return cp.execSync(str, {}).toString();
+	return cp.execSync(str, {cwd: appDir}).toString();
 };
 
 if (process.argv.length <= 2) {
@@ -17,14 +17,18 @@ if (process.argv.length <= 2) {
 
 var appName = process.argv[2];
 var appBranch = process.argv[3];
-var appDir = path.resolve(os.homedir(), `./${appName}`);
+appDir = path.resolve(os.homedir(), `./${appName}`);
 
-out('[1] update code from github');
+out(`[1] update code from ${appName}:${appBranch}`);
 job(`cd ${appDir}`);
 job('git pull');
 job(`git checkout ${appBranch}`);
+job('git pull');
 out('');
 
-//out('[2] pm2 restart app');
-//job(`pm2 restart ${appName}`);
-out('[2] done.');
+//out(`[2] pm2 delete ${appName}`);
+//job(`pm2 delete ${appName}`);
+//job(`pm2 start ${appDir}/${appName}.js`);
+//out('');
+
+out('[3] done.');
